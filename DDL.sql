78e4315d721710242fd32d2137dc7775d3fb62a3
Eva Aiwass <Evawhite400@gmail.com>
2024-01-15 21:08:02 +0700

    Added file

--- DDL.sql
+++ DDL.sql
@@ -1,31 +1,47 @@
-create table  tests (
-    test_id serial primary key,
-    test_name varchar(255) not null
+create table tests
+(
+    test_id             serial primary key,
+    test_name           varchar(255) not null,
+    number_of_questions int          not null,
+    minimal_threshold   int          not null
 );
 
-create table questions (
-    questions_id serial primary key,
-    test_id int references  tests(test_id),
+create table questions
+(
+    questions_id   serial primary key,
+    test_id        int references tests (test_id),
     questions_text TEXT not null
 );
-
-create table answers(
-   answers_id serial primary key,
-   questions_id int references questions(questions_id),
-   answer_text text not null,
-   is_correct boolean not null
+create table answer_options
+(
+    answer_options_id serial primary key,
+    questions_id      int references questions (questions_id),
+    text              text    not null,
+    is_correct        boolean not null
 );
-create table students(
-    student_id serial primary key,
+
+create table student_answers
+(
+    student_answers_id serial primary key,
+    questions_id       int references questions (questions_id),
+    answer_options_id  int references answer_options (answer_options_id),
+    student_id         int references students (student_id)
+);
+create table students
+(
+    student_id         serial primary key,
     student_first_name varchar(255) not null,
-    student_last_name varchar(255) not null
+    student_last_name  varchar(255) not null
 );
 
-create table test_results(
-    result_id serial primary key,
-    test_id int references tests(test_id),
-    student_id int references students(student_id),
-    test_date date not null,
-    foreign key (test_id) references tests(test_id),
-    foreign key (student_id) references students(student_id)
+create table test_results
+(
+    result_id  serial primary key,
+    test_id    int references tests (test_id),
+    student_id int references students (student_id),
+    test_date  date    not null,
+    is_done    boolean not null,
+    foreign key (test_id) references tests (test_id),
+    foreign key (student_id) references students (student_id)
 );
+
--- DML.sql
+++ DML.sql
@@ -1,25 +1,47 @@
-INSERT INTO tests (test_name) VALUES ('Тест по математике');
-INSERT INTO questions (questions_id,test_id, questions_text) VALUES (1, 1, 'Сколько будет 2 + 2?');
-INSERT INTO answers (questions_id, answer_text, is_correct) VALUES (1, '4', TRUE);
-INSERT INTO students (student_id,student_first_name, student_last_name) VALUES (1, 'Иван', 'Иванов');
-INSERT INTO test_results (result_id, test_id, student_id, test_date) VALUES (1,1, 1, '2024-01-11');
+INSERT INTO tests (test_id, test_name, number_of_questions, minimal_threshold)
+VALUES (1, 'Тест по математике', 1, 1);
 
-SELECT
-    tr.result_id,
-    tr.test_date,
-    s.student_first_name,
-    s.student_last_name,
-    t.test_name,
-    q.questions_text,
-    a.answer_text,
-    a.is_correct
-FROM
-    test_results tr
-JOIN
-    students s ON tr.student_id = s.student_id
-JOIN
-    tests t ON tr.test_id = t.test_id
-JOIN
-    questions q ON q.test_id = t.test_id
-JOIN
-    answers a on q.questions_id = a.questions_id;
+INSERT INTO questions (questions_id, test_id, questions_text)
+VALUES (1, 1, 'Сколько будет 2 + 2?');
+
+INSERT INTO answer_options (answer_options_id, questions_id, text, is_correct)
+VALUES (1, 1, '4', TRUE);
+
+INSERT INTO answer_options (answer_options_id, questions_id, text, is_correct)
+VALUES (2, 1, '6', FALSE);
+
+INSERT INTO answer_options (answer_options_id, questions_id, text, is_correct)
+VALUES (3, 1, '3', FALSE);
+
+INSERT INTO students (student_id, student_first_name, student_last_name)
+VALUES (1, 'Иван', 'Иванов');
+
+--Записываем ответ студента 
+INSERT INTO student_answers (student_answers_id, questions_id, answer_options_id, student_id)
+VALUES (1, 1, 1, 1);
+
+--На основании предоставленных ответов формируем результат 
+WITH TEST_INFO AS (
+    SELECT t.test_id as test_id, t.minimal_threshold as minimal_threshold
+    FROM tests t
+    WHERE t.test_id = 1
+),
+ANSWER_SUMMARY AS (
+    SELECT
+        (SELECT count(sa.answer_options_id)
+         FROM student_answers sa
+         JOIN answer_options ao on sa.answer_options_id = ao.answer_options_id
+         JOIN questions q on sa.questions_id = q.questions_id
+         JOIN tests t on q.test_id = t.test_id
+         WHERE t.test_id = 1
+           AND sa.student_id = 1
+           AND ao.is_correct = true) >= TI.minimal_threshold as PASSED
+    FROM TEST_INFO TI
+)
+INSERT INTO test_results(result_id, test_id, student_id, test_date, is_done)
+VALUES (1, 1, 1, '2024-01-11', (SELECT PASSED FROM ANSWER_SUMMARY));
+
+--Выполняем запрос о прохождении теста студентом
+SELECT s.student_first_name, s.student_last_name, tr.is_done, tr.test_date
+FROM test_results tr
+         JOIN students s on tr.student_id = s.student_id
